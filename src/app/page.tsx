c919d3a65cba022f2fa6d08c36fdbde605cf4885
piyush8512 <prajapatipiyush851@gmail.com>
2024-10-30 11:38:44 +0530

    first commit2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,9 @@
-// const HomePage = () => {
-//   return (
-//     <div className=''>
-      
-//     </div>
-//   )
-// }
-
-// export default HomePage
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Lama Dev E-Commerce Application",
-  description: "A complete e-commerce application with Next.js and Wix",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const HomePage = () => {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Navbar/>
-        {children}
-        <Footer/>
-        </body>
-    </html>
-  );
+    <div className=''>
+      hello
+    </div>
+  )
 }
+
+export default HomePage
